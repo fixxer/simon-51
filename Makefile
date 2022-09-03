6022eedf88e6cdca12ad114600a5ac264b4a1417
fixxer <fixxer@inbox.ru>
2022-09-03 11:43:47 +0200

    First push to GH.

---
+++ Makefile
@@ -0,0 +1,50 @@
+MCU_FAMILY = mcs51
+MCU = at89c2051
+CHIP_PACKAGE = DIP20
+
+##########------------------------------------------------------##########
+##########                  Program Locations                   ##########
+##########     Won't need to change if they're in your PATH     ##########
+##########------------------------------------------------------##########
+
+CC = sdcc
+MINIPRO = minipro
+
+## The name of your project (without the .c)
+# TARGET = simon
+## Or name it automatically after the enclosing directory
+TARGET = $(lastword $(subst /, ,$(CURDIR)))
+
+# Object files: will find all .c/.h files in current directory
+#  and in LIBDIR.  If you have any other (sub-)directories with code,
+#  you can add them in to SOURCES below in the wildcard statement.
+SOURCES=$(wildcard *.c $(LIBDIR)/*.c)
+OBJECTS=$(SOURCES:.c=.o)
+HEADERS=$(SOURCES:.c=.h)
+
+TARGET_ARCH = -m$(MCU_FAMILY) -p$(MCU)
+
+%.ihx: %.c Makefile
+	$(CC) $(TARGET_ARCH) $<;
+
+## These targets don't have files named after them
+.PHONY: all clean flash
+
+all: $(TARGET).ihx
+
+debug:
+	@echo
+	@echo "Source files:" $(SOURCES)
+	@echo "Header files:" $(HEADERS)
+	@echo "MCU, MCU_FAMILY, BAUD:"  $(MCU), $(MCU_FAMILY)
+	@echo
+
+clean:
+	rm -f $(TARGET).elf $(TARGET).ihx $(TARGET).obj \
+	$(TARGET).o $(TARGET).d $(TARGET).eep $(TARGET).lst \
+	$(TARGET).lss $(TARGET).sym $(TARGET).map $(TARGET)~ \
+	$(TARGET).eeprom $(TARGET).asm \
+	$(TARGET).lk $(TARGET).mem $(TARGET).rel $(TARGET).rst
+
+flash: $(TARGET).ihx
+	$(MINIPRO) -p $(MCU)@$(CHIP_PACKAGE) -w $<
---
+++ simon.c
@@ -0,0 +1,95 @@
+#include <mcs51/at89x051.h>
+
+#define ASK 0
+#define ANSWER 1
+#define BUTTONS_COUNT 4
+#define CLOCK 6000000
+#define TIMER_SCALE_FACTOR 12
+
+const unsigned char leds[] = {0b10011111,  0b01011111, 0b00111111, 0b11111111};
+const unsigned char button_masks[] = {0b00010000,  0b00001000, 0b00000100, 0b00000010};
+volatile unsigned int millis = 0;
+volatile unsigned char countdowns[] = {0, 0}; 
+volatile __sbit half_millis_counter = 0;
+volatile unsigned char tone_timer_hi = 0;
+volatile unsigned char tone_timer_lo = 0;
+
+/**
+568 - 880 Hz
+*/
+void tone(unsigned int divider, unsigned char duration_ms) {
+  unsigned int timer_init = 0xffff - divider;
+  tone_timer_hi = (unsigned char) (timer_init >> 8);
+  tone_timer_lo = (unsigned char) timer_init;
+  TH0 = tone_timer_hi;
+  TL0 = tone_timer_lo;
+  countdowns[0] = duration_ms;
+  TR0 = 1;
+}
+
+void main() {
+  unsigned char mode = ASK;
+  unsigned char current_led = 0;
+
+  unsigned char button_flags[] = {0, 0, 0, 0};
+
+  // Setup timers
+  TH1 = 0x06;  // Set timer1 value (256 - 6 = 250 count until trigger)
+  TL1 = 0x06;
+  TMOD = 0x21; // Set timer0 mod1, timer1 mod2 
+  TR1 = 1; // Start timer1
+  ET0 = 1; // Enable timer0 interrupt
+  ET1 = 1; // Enable timer1 interrupt
+  EA = 1;  // Enable interrupts
+
+  while(1) {
+    if (mode == ASK) {
+      current_led = TL1 & 0x03; // Use timer counter as a pseudorandom
+      P1 = leds[current_led];
+      mode = ANSWER;
+      tone(568, 255);
+    } else {
+      for (unsigned char i = 0; i < BUTTONS_COUNT; i++) {
+        if (!(P1 & button_masks[i]) && button_flags[i] == 0 && countdowns[1] == 0) {
+          countdowns[1] = 50;
+          button_flags[i] = 1;          
+          if (i == current_led) {
+            //tone(568, 255);
+            tone(568 / 2, 255);
+          } else {
+            //tone(568, 255);
+            tone(568 * 2, 255);
+          }
+        } else if (P1 & button_masks[i] && button_flags[i] == 1 && countdowns[1] == 0) {
+          countdowns[1] = 50;
+          button_flags[i] = 0;          
+          mode = ASK;
+        }
+      }
+    }
+  }
+}
+
+void timer0() __interrupt (TF0_VECTOR) {
+  TR0 = 0; //Disable timer0
+
+  P3_7 ^= 1; // Toggle pin
+
+  TH0 = tone_timer_hi; // Reset timer0 value
+  TL0 = tone_timer_lo;
+
+  if (countdowns[0] > 0) {
+    TR0 = 1; // Enable timer0
+  } else {
+    P3_7 = 0; // Sound off
+  }
+}
+
+void timer1() __interrupt (TF1_VECTOR) {
+  half_millis_counter ^= 1;
+  if (half_millis_counter == 0) {
+    if (countdowns[0] > 0) countdowns[0]--;
+    if (countdowns[1] > 0) countdowns[1]--;
+    millis++;
+  }
+}
